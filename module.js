85e3ef2a8b5dfc445a87277190c0827f3843554a
Soukina-NOUNOU <soukina.nounou@gmail.com>
2026-02-06 12:26:47 +0100

    Add unit tests for calculateAge function and update devDependencies

--- module.js
+++ module.js
@@ -5,8 +5,37 @@
  * @returns {number} The age of the p.
  */
 
-function calculateAge(p) {
-    let dateDiff = new Date(Date.now() - p.birth.getTime());
+export default function calculateAge(p) {
+    if(!p) {
+        throw new Error("missing param p")
+    }
+
+    if(typeof p !== "object") {
+        throw new Error("format is not correct")
+    }
+
+    if(!p.birth) {
+        throw new Error("birth property is missing")
+    }
+
+    if(!(p.birth instanceof Date)) {
+        throw new Error('Not valid birth date')
+    }
+
+    if(isNaN(p.birth.getTime())) {
+        throw new Error('This date is impossible')
+    }
+
+    if(p.birth.getTime() > Date.now()) {
+        throw new Error('It is impossible to be born in the future')
+    }
+
+    if(p.birth.getTime() < new Date('01/01/1970').getTime()) {
+        throw new Error('This date is too far in the past')
+    }
+
+    let dateDiff = new Date(Date.now() - p.birth.getTime())
     let age = Math.abs(dateDiff.getUTCFullYear() - 1970);
     return age;
+
 }
---
+++ test.js
@@ -0,0 +1,65 @@
+import calculateAge from './module';
+/**
+ * @function calculateAge
+ */
+
+let people20years;
+beforeEach(() => {
+    let date = new Date();
+    people20years = {
+        birth: new Date(date.setFullYear(date.getFullYear() - 20))
+    };
+})
+
+describe('calculateAge Unit Test Suites', () => {
+    it('should return  a correct age', () => {
+        const loise = {
+            birth: new Date('08/19/1993')
+        }
+        expect(calculateAge(loise)).toBe(32);
+    });
+
+    it('should throw a "missing param p" error', () => {
+        expect(() => calculateAge()).toThrow("missing param p")
+    })
+
+    it('should throws if parameter is not an object', () => {
+        expect(() => calculateAge("hello")).toThrow("format is not correct")
+    })
+
+    it('should throws if parameter does not have a birth property', () => {
+        expect(() => calculateAge({})).toThrow("birth property is missing")
+    })
+
+    it('should throws if birth is not a Date or invalide date', () => {
+        expect(() => calculateAge({birth: 124578})).toThrow('Not valid birth date')
+    })
+
+    it('sould throws if birth is an impossible date', () => {
+        expect(() => calculateAge({birth: new Date('31/02/1990')})).toThrow('This date is impossible')
+    })
+
+    it('should throws if birth is in the future', () => {
+        expect(() => calculateAge({birth: new Date(Date.now() + 100000)})).toThrow('It is impossible to be born in the future')
+    })
+
+    it('sould throws if birth is too far in the past', () => {
+        expect(() => calculateAge({birth: new Date('01/01/1800')})).toThrow('This date is too far in the past')
+    })
+
+    it('sould correctly calculates age regardless of the current date', () => {
+        const loise = {
+            birth: new Date('08/19/1993')
+        }
+        const realDateNow = Date.now;
+        Date.now = () => new Date('08/19/2020').getTime();
+        expect(calculateAge(loise)).toBe(27);
+        Date.now = realDateNow;
+    })
+
+    it('should return a correct age', () => {
+        expect(calculateAge(people20years)).toEqual(20)
+    })
+
+
+});
