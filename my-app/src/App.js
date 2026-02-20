bbfc51d4b55733576cba3704b5cb6ed321cf440a
Soukina-NOUNOU <soukina.nounou@gmail.com>
2026-02-20 12:53:03 +0100

    fix: update Router component to include basename for proper routing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,7 @@ import "./App.css";
 function App() {
   return (
     <UserProvider>
-      <Router>
+      <Router basename="/Ynov_test">
         <div className="App">
           <Routes>
             <Route path="/" element={<HomePage />} />
